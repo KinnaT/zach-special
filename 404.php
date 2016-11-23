a28e96b885c22f59a542efd5c99aaad6765f4f10
Kinna <kinna3@outlook.com>
2016-11-23 14:16:30 -0600

    Another surprise! Fixing formatting

--- 404.php
+++ 404.php
@@ -3,8 +3,8 @@
     <div class="center">
     <h1>It's dead, Jim.</h1>
     </div>
+</div>
 <div class="sidebar">
     <p class="saveme"><a href="javascript:history.go(-1)"><span class="dashicons dashicons-undo"></span>Return to safety</a></p>
-</div>
 </div>
 <?php get_footer(); ?>
