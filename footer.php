0e34b80d49421097e4191ed584b5c4b6da1adaa5
Zach Schneider <me@zachschneider.com>
2016-11-23 11:52:51 -0600

    making it mobile

--- footer.php
+++ footer.php
@@ -1,3 +1,14 @@
   <?php wp_footer(); ?>
+    <footer id="colophon" class="site-footer" role="contentinfo">
+
+      <?php get_sidebar( 'footer' ); ?>
+
+      <div class="site-info">
+
+      </div><!-- .site-info -->
+    </footer><!-- #colophon -->
+  </div><!-- #page -->
+
+  <?php wp_footer(); ?>
 </body>
 </html>
--- header.php
+++ header.php
@@ -1,14 +1,16 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-    <head>
-        <meta charset="<?php bloginfo( 'charset' ); ?>" />
-        <title><?php wp_title(); ?></title>
-        <link rel="profile" href="http://gmpg.org/xfn/11" />
-        <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-        <?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); ?>
-        <?php wp_head(); ?>
-    </head>
-    <body class="special">
-    <div class="header">
-        <div class="nav"><?php wp_nav_menu(); ?></div>
-    </div>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width">
+    <title><?php wp_title( '|', true, 'right' ); ?></title>
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+    <?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); ?>
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+    <div id="page">
+        <div class="header">
+            <div class="nav"><?php wp_nav_menu(); ?></div>
+        </div>
