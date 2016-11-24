0794bd115a581256789f52151f8db1ff189dd7d2
Zach Schneider <me@zachschneider.com>
2016-11-24 13:36:40 -0600

    all kinds of things

--- footer.php
+++ footer.php
@@ -1,15 +1,15 @@
+    </div>
     <footer id="colophon" class="site-footer" role="contentinfo">
       <div class="site-info">
-        <?php
-          /**
-           * Fires before the twentysixteen footer text for footer customization.
-           *
-           * @since Twenty Sixteen 1.0
-           */
-          do_action( 'twentysixteen_credits' );
-        ?>
-        <span class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></span>
+        <span class="site-title">
+          <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+        </span>
         <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentysixteen' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentysixteen' ), 'WordPress' ); ?></a>
+
+        <span class="copyright">
+          &copy; 2006 – <?php echo date('Y'); ?>
+        </span>
+
       </div><!-- .site-info -->
     </footer><!-- .site-footer -->
   </div><!-- .site-inner -->
--- page.php
+++ page.php
@@ -1,13 +1,12 @@
 <?php get_header(); ?>
-<div class="content page">
-  <?php
-  if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-    <div class="page-content">
-      <?php the_content(); ?>
+  <div id="primary" class="page-content">
+    <div id="content" role="main">
+      <?php
+      if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+        <div class="page-content">
+          <?php the_content(); ?>
+        </div>
+      <?php endwhile; endif; ?>
     </div>
-  <?php endwhile; endif; ?>
-</div>
-<div class="sidebar">
-<?php get_sidebar(); ?>
-</div>
+  </div>
 <?php get_footer(); ?>
